d3a02b1fe75df7ccda5da6c696accc3839c2600d
Mhizta-Tony <okanaanthony12@gmail.com>
2022-09-11 14:10:49 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,10 @@ int main(void)
 {
 	char c;
 
-        for (c = '0'; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
-
 	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
@@ -23,6 +22,3 @@ int main(void)
 
 	return (0);
 }
-	
-	
-	
