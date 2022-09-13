8d8f13f4b0548e3ed92c56a7f3aa87caf4475adf
Mhizta-Tony <okanaanthony12@gmail.com>
2022-09-13 15:56:45 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet – function to print abc
+ *
+ * Return: 0
+ */
+Void print_alphabet(void)
+{
+	Char c;
+
+	For (c = ‘a’; c <= ‘z’; c++)
+	{
+		_putchar(c);
+	}
+_putchar(‘\n’);
+}
