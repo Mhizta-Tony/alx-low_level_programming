f19b4dc23538040e8d2f92563e2388a2c5bba446
Mhizta-Tony <okanaanthony12@gmail.com>
2022-09-14 01:55:22 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
 /**
- * print_alphabet –> prints the lower
+ * print_alphabet – function to print abc
  *
+ * Return: 0
  */
-
 void print_alphabet(void)
 {
-	int j;
+	char c;
 
-	for (j = 'a'; j <= 'z'; j++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(j);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
